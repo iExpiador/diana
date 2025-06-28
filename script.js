fa48719ae0f10282bc3b0c9cb6ce96a0e67f3b04
iExpiador <viruswebmail@gmail.com>
2025-06-28 16:36:27 -0500

    Update script.js
    canciones

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".btn.forum").addEventListener("click", () => {
-    window.open("https://blackscreen.app/", "_blank");
+    window.open("https://www.youtube.com/watch?v=vs61OHs2g-w", "_blank");
   });
 
   document.querySelector(".btn.deseados").addEventListener("click", () => {
-    window.open("https://www.nyan.cat/", "_blank");
+    window.open("https://www.youtube.com/watch?v=-qvYXZOUImo", "_blank");
   });
 
   document.querySelector(".btn.blog").addEventListener("click", () => {
-    window.open("http://xat.me/Akiiimitsu", "_blank");
+    window.open("https://www.youtube.com/watch?v=VSXT4a2kRHA", "_blank");
   });
 
   console.log("Dark profile card loaded.");
